571c0aa7ff0840713a1b746a709477ed61e94ab9
joseftango <4279@holbertonschool.com>
2024-01-13 15:47:07 +0100

    task_0

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Header, Body, Footer} from "./components/layout";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
