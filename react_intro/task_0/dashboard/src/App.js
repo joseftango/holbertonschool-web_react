0aa2f79b734167d1390ff15c89be070ff55b8614
joseftango <4279@holbertonschool.com>
2024-01-13 16:24:57 +0100

    task_0

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import {Header, Body, Footer} from "./components/layout";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Body></Body>
-      <Footer></Footer>
+      <div className="App-header">
+			<img src='https://s3.eu-west-3.amazonaws.com/hbtn.intranet/uploads/medias/2019/11/175b85183ecedb529e14.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4MYA5JM5DUTZGMZG%2F20240113%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20240113T131429Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=4befd9346684a2138048b4a9dc182beb9cbd09b47daa9fa9814df6743f48ce53' alt="Holberton Logo" />
+			<h1>School dashboard</h1>
+		  </div>
+      <div className="App-body"><p>Login to access the full dashboard</p></div>
+      <div className="App-footer"><p>Copyright 2020 - holberton School</p></div>
     </div>
   );
 }
