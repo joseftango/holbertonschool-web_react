ddfd83ef313c839f67c29594e75324c3868d9287
joseftango <4279@holbertonschool.com>
2024-01-16 18:30:52 +0100

    task_5

--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -1,2 +1,18 @@
-const message = 'Hello, Webpack!';
-console.log(message);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App/App';
+import Notifications from './Notifications/Notifications';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>,
+  document.getElementById('root-notifications')
+);
