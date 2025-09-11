d5c75cbd754e5d935a8b80b7bb0c7a3b8aa00fb4
joseftango <4279@holbertonschool.com>
2025-09-11 04:16:30 +0100

    Webpack

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,22 +1,21 @@
-import $ from 'jquery';
-const _ = require('lodash');
-
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
-
-$('button').on(
-  'click',
-  _.debounce(updateCounter, 500, {
-    leading: true,
-    trailing: false,
-  })
-);
+import $ from "jquery";
+import _ from "lodash";
 
 let count = 0;
+
 function updateCounter() {
-  count++;
-  $('#count').text(`${count} clicks on the button`);
+    count++;
+    $("#count").text(`${count} clicks on the button`);
 }
+
+const debouncedUpdate = _.debounce(updateCounter, 500);
+
+$(function () {
+    const body = $('body');
+    body.append('<p>Holberton Dashboard</p>');
+    body.append('<p>Dashboard data for the students</p>');
+    body.append('<button>Click here to get started</button>');
+    body.append('<p id="count"></p>');
+    body.append('<p>Copyright - Holberton School</p>');
+    $('button').on('click', debouncedUpdate);
+});
